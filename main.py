fe45e137efece632d0fa0f62384b78e42b736bf2
panka134 <134panka@gmail.com>
2022-01-07 22:28:03 +0100

    Clean up and add todo list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#TODO: 
+#TODO:
 # - dwa dźwięki na raz (numer_dźwięku)
-# - niskopoziomowe
-# - przerwanie danego trybu i powrót do menu
-
+# klawisze, LCD
 
 import RPi.GPIO as GPIO
 import time
@@ -13,14 +11,17 @@ from consts import *
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-Buzz = None
+buzzer = None
 option_chosen = 0
 current_menu = 0  # 0 - main menu, 1 - songs menu (to play), 2 - songs menu (to learn)
 conn = None # db connection
+max_song_id = None
 
-# ------------------------ DATABASE--------------------------------------------------
+# ------------------------ DATABASE---------------------------------------------
 
 def create_connection(db_file):
+    """Connect to database"""
+
     global conn
     try:
         conn = sqlite3.connect(db_file)
@@ -31,30 +32,58 @@ def create_connection(db_file):
         show_text_lcd(e)
         return None
 
-#--------------------------------EMBEEDED-------------------------------------------------
+
+def get_max_song_id():
+    """How many songs in the database"""
+
+    global max_song_id
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT id FROM songs ORDER BY id LIMIT 1')
+    max_song_id = cursor.fetchall()[0][0]
+    cursor.close()
+    return max_song_id
+
+
+def get_song(song_id):
+    """Get all sounds in the song"""
+
+    global conn
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT sound_name, sound_length FROM Sounds_in_songs WHERE song_id = {song_id} ORDER BY sound_number')
+    song = cursor.fetchall()
+    cursor.close()
+    return song
+
+#--------------------------------EMBEEDED---------------------------------------
 
 def init():
-    global Buzz
+    """"Initialize GPIO pins and all devices"""
+
+    global buzzer
     for pin in pins_out.keys():
         GPIO.setup(pins_out[pin], GPIO.OUT)
     for button in buttons.keys():
         GPIO.setup(buttons[button], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # an input pin, set initial value to be pulled low (off)
+
     GPIO.add_event_detect(buttons["up"], GPIO.RISING, callback = on_click_button_up) # Setup event on rising edge
     GPIO.add_event_detect(buttons["down"], GPIO.RISING, callback = on_click_button_down)
     GPIO.add_event_detect(buttons["ok"], GPIO.RISING, callback = on_click_button_ok)
 
-    Buzz = GPIO.PWM(pins_out["BUZZER"], 440)    
-    Buzz.start(50)
+    buzzer = GPIO.PWM(pins_out["BUZZER"], 440)
+    buzzer.start(50)
 
 
-def play_tone(frequency): 
-    global Buzz
-    Buzz.duty_u16(1000)
-    Buzz.freq(frequency)
+def play_tone(frequency):
+    """Play tone on buzzer"""
+    global buzzer
+    buzzer.duty_u16(1000)
+    buzzer.freq(frequency)
 
 
 def be_quiet():
-    Buzz.duty_u16(0)
+    """Turn the buzzer off"""
+    global buzzer
+    buzzer.duty_u16(0)
 
 
 def light_diode(addr: list):
@@ -67,12 +96,14 @@ def light_diode(addr: list):
 
 
 def light_off():
+    """Turn off all diodes"""
+
     GPIO.output(pins_out["MAIN_MUX_Y"], GPIO.LOW)
     GPIO.output(pins_out["MAIN_MUX_X"], GPIO.LOW)
 
 
 def play_sound(note: str, beat = 0.01):
-    global Buzz
+    """Play tone on the buzzer and light the diode for certain time"""
     light_diode(diodes[note])
     play_tone(sounds[note]) #TODO: play 2 sounds
     time.sleep(beat*0.13)
@@ -80,13 +111,8 @@ def play_sound(note: str, beat = 0.01):
     light_off()
 
 
-def get_button(button: str):
-    #TODO: check on gpio
-    pass
-
-
 def show_text_lcd(text: str):
-    print(text)
+    print("ON LCD:", text)
     #TODO: show on LCD
     pass
 
@@ -95,7 +121,7 @@ def get_key():
     #TODO: check on gpio
     pass
 
-# ----------------------------------- HELPFUL -----------------------------------------------------------
+# ----------------------------------- HELPFUL ----------------------------------
 
 def on_click_button_up():
     global option_chosen
@@ -112,16 +138,8 @@ def on_click_button_down():
 def on_click_button_ok():
     if current_menu == 0:
         choose_option_main_menu()
-    elif current_menu == 1:
+    elif current_menu in (1,2):
         choose_option_songs_menu()
-
-
-
-def check_buttons():
-    for button in buttons.keys():
-        if get_button(button):
-            return button
-    return None
 
 
 def check_keys():
@@ -139,28 +157,21 @@ def free_play():
     while True:
         note = check_keys()
         play_sound(note)
-    
 
-def play_song(song_id, conn):
+
+def play_song(song_id):
     """Play song and light diodes without user's interactions"""
-
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT sound_name, sound_length FROM Sounds_in_songs WHERE song_id = {song_id} ORDER BY sound_number')
-    song = cursor.fetchall()
-    cursor.close()
+    song = get_song(song_id)
 
     for note in song:
         play_sound(*note)
 
 
-def learn_song(song_id, conn):
+def learn_song(song_id):
     """Light diodes only for sounds which should be played in right order"""
 
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT sound_name, sound_length FROM Sounds_in_songs WHERE song_id = {song_id} ORDER BY sound_number')
-    song = cursor.fetchall()
-    cursor.close()
-    
+    song = get_song(song_id)
+
     for note in song:
         light_diode(diodes[note[0]])
         while check_keys() != note[0]: pass
@@ -170,92 +181,77 @@ def learn_song(song_id, conn):
 
 def shutdown_piano():
     """"Clean and exit"""
-    #TODO: close GPIO
+    GPIO.cleanup()
+    conn.close()
     show_text_lcd("Goodbye!")
     time.sleep(2)
     #TODO: shutdown pi
-    pass
 
 
 # ------------------------------------MENU(S)----------------------------------------------------
 def refresh_menu():
     if current_menu == 0: refresh_main_menu()
     elif current_menu == 1: refresh_songs_menu()
-
-
-
-def refresh_songs_menu(conn):
-    """ Select songs from Songs database """
-    
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT title FROM Songs')
-    names = cursor.fetchall()
-    
-    option_chosen = 0
-    
-    while True:
-        
-        songs_size = len(names)
-
-        print(names[option_chosen])
-        print(names[(option_chosen+1)%songs_size])
-        
-        button = None
-        while not button:
-            button = check_buttons()
-
-        if button == "up": option_chosen += 1
-        elif button == "down": option_chosen -= 1
-        elif button == "ok": 
-            cursor.execute(f'SELECT id FROM Songs WHERE title = {names[option_chosen]}')
-            song_id = cursor.fetch()
-            cursor.close()
-            return song_id
-
-
-def choose_option_main_menu():
-    if option_chosen == 0: free_play()
-
-    elif option_chosen == 1: 
-        current_menu = 1
-
-    elif option_chosen == 2: 
-        current_menu = 2
-    else: shutdown_piano()    
-
-
-def choose_option_songs_menu():
-    global conn
-    cursor = conn.cursor()
-    cursor.execute(f'SELECT id FROM Songs WHERE title = {names[option_chosen]}') #TODO: brak listy piosenek
-    song_id = cursor.fetch()
-    cursor.close()
-    if current_menu == 1:
-        play_song(song_id, conn)
-    elif current_menu == 2:
-        learn_song(song_id, conn)
-    return song_id
-
+    elif current_menu == 2: refresh_songs_menu()
 
 
 def refresh_main_menu(conn):
     """Wyświetl opcje i czekaj na naciśnięcie przycisku"""
     global option_chosen
     options = ["Graj sam", "Odtwórz melodię", "Naucz się grać", "Wyjście"]
-        
+    option_chosen = option_chosen % 4
+
     show_text_lcd("< " + options[option_chosen] + " >")
     show_text_lcd(options[(option_chosen+1)%4])
 
 
-if __name__ == "__main__":
+def refresh_songs_menu(conn):
+    """ Select songs from Songs database """
+
+    global option_chosen
+    option_chosen = option_chosen % max_song_id
+    next_option = (option_chosen + 1) % max_song_id
+
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT title FROM Songs WHERE id IN ({option_chosen}, {next_option})')
+    names = cursor.fetchall()
+
+    show_text_lcd(names[0])
+    show_text_lcd(names[1])
+
+
+def choose_option_main_menu():
+    if option_chosen == 0: free_play()
+
+    elif option_chosen == 1:
+        current_menu = 1
+
+    elif option_chosen == 2:
+        current_menu = 2
+    else: shutdown_piano()
+
+
+def choose_option_songs_menu():
+    if current_menu == 1:
+        play_song(option_chosen)
+    elif current_menu == 2:
+        learn_song(option_chosen)
+
+#------------------------------------------------------------------------------
+
+def main():
     show_text_lcd("Dzien dobry!")
     #TODO: light all diodes in order
     time.sleep(2)
     conn = create_connection("piano.db")
-    if conn == None: 
+    if conn == None:
         show_text_lcd("Brak bazy piosenek")
         free_play()
     else:
         with conn:
-            show_menu(conn)
+            refresh_main_menu()
 
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,7 +1,31 @@
+"""
+przetestowane:
++ get_max_song_id
++ łączenie się z bazą
+
+#TODO:
+
+przygotować funkcje do testowania hardware'u:
+- zapalanie i gaszenie diody (to chyba działało)
+- show_text_lcd
+- zmianę zmiennej przy naciskaniu przycisków up/ down
+
+- play_tone i be_quiet
+- play_sound (dioda i buzzer)
+- play_song
+
+- logi (printy) w menu
+
+- naciskanie klawiszy (na razie nie ma nic)
+- free_play
+- learn_song
+"""
+
 import sqlite3
 from sqlite3 import Error
 from consts import *
 
+max_song_id = None
 
 def create_connection(db_file):
     try:
@@ -13,6 +37,15 @@ def create_connection(db_file):
         show_text_lcd(e)
         return None
 
+def get_max_song_id():
+    """How many songs in the database"""
+
+    global max_song_id
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT id FROM songs ORDER BY id LIMIT 1')
+    max_song_id = cursor.fetchall()[0][0]
+    cursor.close()
+    return max_song_id
 
 def play_song(song_id, conn):
     """Play song and light diodes without user's interactions"""
@@ -20,12 +53,9 @@ def play_song(song_id, conn):
     cursor.execute(f'SELECT sound_name, sound_length FROM Sounds_in_songs WHERE song_id = {song_id} ORDER BY sound_number')
     song = cursor.fetchall()
     print(song)
-    
-    #TODO: song = wyciągnij info z bazy Dzwieki_w_piosenkach (nazwa, długość)
-    # for note in song:
-    #     play_sound(note)
 
 
 conn = create_connection("piano.db")
 with conn:
-    play_song(0, conn)
+    print(get_max_song_id())
+    print(max_song_id)
