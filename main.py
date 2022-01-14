4b87896acd349c8d4c38da58842daa24ffe0398d
panka134 <134panka@gmail.com>
2022-01-14 21:08:14 +0100

    Diodes and lcd working

--- main.py
+++ main.py
@@ -1,19 +1,163 @@
-#TODO:
+# TODO:
 # - dwa dźwięki na raz (numer_dźwięku)
 # klawisze
 
 import time
 from consts import *
-from low_level import *
+# from low_level import *
 from database import *
 
-buzzer = None
-option_chosen = 0
-current_menu = 0  # 0 - main menu, 1 - songs menu (to play), 2 - songs menu (to learn)
-conn = None # db connection
-max_song_id = None
+import RPi.GPIO as GPIO
+from RPLCD import CharLCD
+from consts import *
 
-#----------------------------------THE VIP FUNCTIONS-----------------------------------------------------------
+DEBUG = 1  # enable console output
+
+buzzer = None
+current_menu = 0  # 0 - main menu, 1 - songs menu (to play), 2 - songs menu (to learn)
+conn = None  # db connection
+max_song_id = None
+cap = MPR121.MPR121()
+
+
+# --------------------------------EMBEEDED---------------------------------------
+
+def init():
+    """"Initialize GPIO pins and all devices"""
+    # global buzzer
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    for pin in pins_out.keys():
+        GPIO.setup(pins_out[pin], GPIO.OUT)
+    for button in buttons.keys():
+        GPIO.setup(buttons[button], GPIO.IN,
+                   pull_up_down=GPIO.PUD_UP)  # an input pin, set initial value to be pulled low (off)
+
+    GPIO.add_event_detect(buttons["up"], GPIO.FALLING, callback=on_click_button_up)  # Setup event on rising edge
+    GPIO.add_event_detect(buttons["down"], GPIO.FALLING, callback=on_click_button_down)
+    GPIO.add_event_detect(buttons["ok"], GPIO.FALLING, callback=on_click_button_ok)
+
+    # buzzer = GPIO.PWM(pins_out["BUZZER"], 440)
+    # buzzer.start(50)
+    if DEBUG: print("Initialization completed successfully")
+
+
+def play_tone(frequency):
+    """Play tone on buzzer"""
+    print("play_tone", frequency)
+    # global buzzer
+    # buzzer.duty_u16(1000)
+    # buzzer.freq(frequency)
+
+
+def be_quiet():
+    """Turn the buzzer off"""
+    print("quiet")
+    # global buzzer
+    # buzzer.duty_u16(0)
+
+
+def light_diode(addr: list):
+    if DEBUG: print("diode", addr, "is on")
+    if addr[0] == 'X':
+        GPIO.output(pins_out["MAIN_MUX_Y"], GPIO.LOW)
+        GPIO.output(pins_out["MAIN_MUX_X"], GPIO.HIGH)
+    elif addr[0] == 'Y':
+        GPIO.output(pins_out["MAIN_MUX_Y"], GPIO.HIGH)
+        GPIO.output(pins_out["MAIN_MUX_X"], GPIO.LOW)
+
+    GPIO.output(pins_out["MAIN_MUX_ADDR_A"], addr[1])
+    GPIO.output(pins_out["MAIN_MUX_ADDR_B"], addr[2])
+    GPIO.output(pins_out["ALL_MUX_ADDR_A"], addr[3])
+    GPIO.output(pins_out["ALL_MUX_ADDR_B"], addr[4])
+
+
+def light_off():
+    """Turn off all diodes"""
+
+    if DEBUG: print("diodes are off")
+    GPIO.output(pins_out["MAIN_MUX_Y"], GPIO.LOW)
+    GPIO.output(pins_out["MAIN_MUX_X"], GPIO.LOW)
+
+
+def play_sound(note: str, beat=0.01):
+    """Play tone on the buzzer and light the diode for certain time"""
+
+    light_diode(diodes[note])
+    play_tone(sounds[note])  # TODO: play 2 sounds
+    if DEBUG: print("playing sound: ", note)
+    time.sleep(beat * 0.13)
+    be_quiet()
+    light_off()
+
+
+def show_text_lcd(text: str, line=1):
+    """Show text on lcd (accepts \n\r)"""
+
+    lcd = CharLCD(numbering_mode=GPIO.BCM, cols=16, rows=2, pin_rs=pins_out["LCD_RS"], pin_e=pins_out["LCD_E"],
+                  pins_data=[pins_out["LCD_D4"], pins_out["LCD_D5"], pins_out["LCD_D6"], pins_out["LCD_D7"]])
+
+    lcd.cursor_pos = (line, 0)
+    lcd.write_string(text)
+    if DEBUG: print("ON LCD:", text)
+
+
+def get_key():
+    # TODO: check on gpio
+
+    # return "C1"
+    return None
+
+
+# ----------------------------------- HELPFUL ----------------------------------
+
+def on_click_button_up(arg):
+    global option_chosen
+    if DEBUG: print("Button UP pressed")
+    option_chosen -= 1
+    refresh_menu()
+
+
+def on_click_button_down(arg):
+    global option_chosen
+    if DEBUG: print("Button DOWN pressed")
+    option_chosen += 1
+    refresh_menu()
+
+
+def on_click_button_ok(arg):
+    if DEBUG: print("Button OK pressed")
+    if current_menu == 0:
+        choose_option_main_menu()
+    elif current_menu in (1, 2):
+        choose_option_songs_menu()
+
+
+def check_keys():
+    # if DEBUG: print("Checking keys...")
+    # for key in piano_keys.keys():
+    #     if get_key(key):
+    #         return key
+    # return None
+
+    last_touched = cap.touched()
+    while True:
+
+        current_touched = cap.touched()
+        for i in range(12):
+            pin_bit = 1 << i
+            if current_touched & pin_bit and not last_touched & pin_bit:
+                print('{0} touched!'.format(i))
+
+            if not current_touched & pin_bit and last_touched & pin_bit:
+                print('{0} released!'.format(i))
+
+        last_touched = current_touched
+        time.sleep(0.1)
+
+# ----------------------------------THE VIP FUNCTIONS-----------------------------------------------------------
 
 def free_play():
     """Check which key is pressed and play it"""
@@ -49,24 +193,27 @@ def shutdown_piano():
     conn.close()
     show_text_lcd("Goodbye!")
     time.sleep(2)
-    #TODO: shutdown pi
+    # TODO: shutdown pi
 
 
 # ------------------------------------MENU(S)----------------------------------------------------
 def refresh_menu():
-    if current_menu == 0: refresh_main_menu()
-    elif current_menu == 1: refresh_songs_menu()
-    elif current_menu == 2: refresh_songs_menu()
+    if current_menu == 0:
+        refresh_main_menu()
+    elif current_menu == 1:
+        refresh_songs_menu()
+    elif current_menu == 2:
+        refresh_songs_menu()
 
 
-def refresh_main_menu(conn):
+def refresh_main_menu():
     """Wyświetl opcje i czekaj na naciśnięcie przycisku"""
     global option_chosen
-    options = ["Graj sam", "Odtwórz melodię", "Naucz się grać", "Wyjście"]
+    options = ["Graj sam", "Odtworz melodie", "Naucz sie grac", "Wyjscie"]
     option_chosen = option_chosen % 4
 
     show_text_lcd("< " + options[option_chosen] + " >")
-    show_text_lcd(options[(option_chosen+1)%4])
+    show_text_lcd(options[(option_chosen + 1) % 4])
 
 
 def refresh_songs_menu(conn):
@@ -85,14 +232,16 @@ def refresh_songs_menu(conn):
 
 
 def choose_option_main_menu():
-    if option_chosen == 0: free_play()
+    if option_chosen == 0:
+        free_play()
 
     elif option_chosen == 1:
         current_menu = 1
 
     elif option_chosen == 2:
         current_menu = 2
-    else: shutdown_piano()
+    else:
+        shutdown_piano()
 
 
 def choose_option_songs_menu():
@@ -101,9 +250,14 @@ def choose_option_songs_menu():
     elif current_menu == 2:
         learn_song(option_chosen)
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+
+
+# ---------------------------
 
 def main():
+    init()
     show_text_lcd("Dzien dobry!")
 
     # wave on diodes
@@ -119,13 +273,12 @@ def main():
     else:
         with conn:
             refresh_main_menu()
+            while (True):
+                pass
+
 
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    init()
-    # for diode in diodes.values():
-    #     light_diode(diode)
-    #     time.sleep(1.5)
-    # light_off()
-    show_text_lcd("hello lerld")
+    main()
+
